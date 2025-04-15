90cbe6cab5b19400c90b2634dcb2785595ac2ea0
Aaron Watters <awatters@flatironinstitute.org>
2025-04-15 12:18:46 -0400

    bugfixes and more tests

--- src/index.ts
+++ src/index.ts
@@ -3,67 +3,58 @@
 // export a name string to test module structure
 export const name = 'prefetchjs';
 
-type PromiseResponse = Promise<Response> | null;
+type PromiseBuffer = Promise<ArrayBuffer> | null;
 
 // prefetcher for a list of urls by index
 export class PreFetcher {
     url_prefix: string;
     url_suffixes: string[];
-    fetch_promises: PromiseResponse[];
+    buffer_promises: PromiseBuffer[];
 
     constructor(url_prefix: string, url_suffixes: string[]) {
         this.url_prefix = url_prefix;
         this.url_suffixes = url_suffixes;
-        this.fetch_promises = [];
+        this.buffer_promises = [];
         for (let i = 0; i < url_suffixes.length; i++) {
-            this.fetch_promises.push(null);
+            this.buffer_promises.push(null);
         }
     };
 
     // fetch a url by index
-    fetch_by_index(index: number): PromiseResponse {
+    async fetch_by_index(index: number): Promise<ArrayBuffer> {
         // error if the index is out of bounds
         const length = this.url_suffixes.length;
         if (index < 0 || index >= length) {
             console.log('Index out of bounds', index, length);
             throw new Error('Index out of bounds: ' + index);
         }
-        if (this.fetch_promises[index] === null) {
-            this.fetch_promises[index] = fetch(this.url_prefix + this.url_suffixes[index]);
-        }
-        return this.fetch_promises[index];
-    };
-
-    // fetch an index, prefetch the next, forget the previous
-    fetch_next(index: number): PromiseResponse | null {
-        // error if the index is out of bounds
-        const length = this.url_suffixes.length;
-        const fps = this.fetch_promises;
-        if (index < 0 || index >= length) {
-            console.log('Index out of bounds', index, length);
-            throw new Error('Index out of bounds: ' + index);
-        }
-        if (fps[index] === null) {
-            this.fetch_promises[index] = fetch(this.url_prefix + this.url_suffixes[index]);
-        }
-        if ((index + 1 < length) && (fps[index + 1] === null)) {
-            this.fetch_promises[index + 1] = fetch(this.url_prefix + this.url_suffixes[index + 1]);
-        }
-        if (index - 1 >= 0) {
-            this.fetch_promises[index - 1] = null;
-        }
-        return this.fetch_promises[index];
-    };
-
-    // fetch content as array buffer (with prefetch)
-    async fetch_buffer(index: number): Promise<ArrayBuffer | null> {
-        // error if the index is out of bounds
-        const response = await this.fetch_next(index);
-        if (response === null) {
-            return null;
+        const url = this.url_prefix + this.url_suffixes[index];
+        //.log('fetching url', url);
+        const response = await fetch(url);
+        if (response.status !== 200) {
+            console.log('Error fetching url', url, response.status);
+            throw new Error('Error fetching url: ' + url);
         }
         const buffer = await response.arrayBuffer();
         return buffer;
+    };
+
+    // fetch an index, prefetch the next, forget the previous
+    fetch_buffer(index: number): PromiseBuffer {
+        // error if the index is out of bounds
+        const length = this.url_suffixes.length;
+        const bps = this.buffer_promises;
+        if (bps[index] === null) {
+            bps[index] = this.fetch_by_index(index);
+        }
+        if ((index + 1 < length) && (bps[index + 1] === null)) {
+            bps[index+1] = this.fetch_by_index(index+1);
+        }
+        // free previous buffer
+        if (index - 1 >= 0) {
+            bps[index - 1] = null;
+        }
+        return bps[index];
     };
 };
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -11,9 +11,12 @@ function stubFetches() {
   vi.stubGlobal(
     'fetch', 
     vi.fn((url: string) => {
+      //console.log('mock fetch', url);
       return new Promise((resolve) => {
         setTimeout(() => {
-          const response = new Response(JSON.stringify({ id: url.split('/').pop() }));
+          const id = url.split('/').pop();
+          //console.log('mock fetching', url, id);
+          const response = new Response(JSON.stringify({ id: id}));
           resolve(response);
         }, 100);
       });
@@ -34,20 +37,21 @@ describe('Basics', () => {
   it ('should fetch some urls', async () => {
     stubFetches();
     const pf = new PreFetcher('https://jsonplaceholder.typicode.com/posts/', ['111', '222', '333']);
+    const bps = pf.buffer_promises;
     const response = await pf.fetch_buffer(0);
-    expect(pf.fetch_promises[0]).toBeInstanceOf(Promise);
-    expect(pf.fetch_promises[1]).toBeInstanceOf(Promise);
+    expect(bps[0]).toBeInstanceOf(Promise);
+    expect(bps[1]).toBeInstanceOf(Promise);
     expect(response).toBeInstanceOf(ArrayBuffer);
     const str = new TextDecoder().decode(response!);
-    console.log('response', str);
+    //console.log('response', str);
     expect(str).toBe(JSON.stringify({ id: '111' }));
     const response2 = await pf.fetch_buffer(1);
     expect(response2).toBeInstanceOf(ArrayBuffer);
     const str2 = new TextDecoder().decode(response2!);
-    console.log('response2', str2);
+    //console.log('response2', str2);
     expect(str2).toBe(JSON.stringify({ id: '222' }));
-    expect(pf.fetch_promises[2]).toBeInstanceOf(Promise);
-    expect(pf.fetch_promises[1]).toBeInstanceOf(Promise);
-    expect(pf.fetch_promises[0]).toBeNull();
+    expect(bps[2]).toBeInstanceOf(Promise);
+    expect(bps[1]).toBeInstanceOf(Promise);
+    expect(bps[0]).toBeNull();
   });
 });
